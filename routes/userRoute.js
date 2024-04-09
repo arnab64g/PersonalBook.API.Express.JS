19c264cc509cff963403cfbc06680e35b7e3e6a2
Arnab Goswami <79049651+arnab64g@users.noreply.github.com>
2024-04-09 18:44:09 +0600

    Upda

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,7 +2,7 @@ const bodyParser = require("body-parser");
 const router = require("express").Router();
 const userController = require("../controller/userController");
 const {verifyToken} = require("../services/authService");
-const { roleChange, getall } = require("../temp");
+
 router.use(bodyParser.json())
 
 router.get('/', verifyToken, async (req, res) => {
@@ -23,13 +23,5 @@ router.post('/login', async(req, res) => {
 
     res.json(result);
 }); 
-
-
-router.get('/test', async(req, res) =>{
-    let result = await roleChange();
-    let usrs =  await getall()
-    console.log(result, usrs);
-    res.json({res : result, users : usrs});
-})
 
 module.exports = router;
